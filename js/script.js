c35d102098a6dbc74fd68407b0c95d4adacc4adb
Porkenvolf <89871231+porkenvolf@users.noreply.github.com>
2023-06-13 00:16:01 -0300

    Big commit, see message
    - Set up basic style
    - Implement modal
    - Implement basic addBook functionality, needs aria and data validation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-let myLibrary = [new Book("Dude", "Lets roll", "not read yet")];
+let myLibrary = [
+    new Book("Dude", "Lets roll", "not read yet"),
+    new Book("Dude2", "Lets roll2", "not read yet2"),
+    new Book("Dude", "Lets roll", "not read yet"),
+    new Book("Dude2", "Lets roll2", "not read yet2"),
+    new Book("Dude", "Lets roll", "not read yet"),
+    new Book("Dude2", "Lets roll2", "not read yet2"),
+    new Book("Dude", "Lets roll", "not read yet"),
+    new Book("Dude2", "Lets roll2", "not read yet2"),
+];
 
 function Book(author, title, status) {
     // the constructor...
@@ -8,7 +17,6 @@ function Book(author, title, status) {
 }
 
 function addBookToLibrary(author, title, status) {
-    // do stuff here
     myLibrary.push(new Book(author, title, status));
 }
 function removeBookFromLibrary() {}
@@ -17,11 +25,75 @@ function updateBook() {}
 /* 
 █ █ ▄▀▀ ██▀ █▀▄    █ █▄ █ ▀█▀ ██▀ █▀▄ █▀ ▄▀▄ ▄▀▀ ██▀ 
 ▀▄█ ▄█▀ █▄▄ █▀▄    █ █ ▀█  █  █▄▄ █▀▄ █▀ █▀█ ▀▄▄ █▄▄  */
+const container = document.querySelector(".container");
+const table = document.querySelector("#books");
+
 function displayBooks() {
-    /* Write a function that loops through the array and displays 
-    each book on the page. You can display them in some sort of
-    table, or each on their own “card”. It might help for now to
-    manually add a few books to your array so you can see the display. */
-    
+    //CLEAR
+    table.innerText = "";
+
+    //TABLE HEADERS
+    const keys = Object.keys(new Book());
+    keys.forEach((element) => {
+        const th = document.createElement("th");
+        th.innerText = element.toUpperCase();
+        table.appendChild(th);
+    });
+
+    //TABLE ELEMENTS
+    if (myLibrary.length > 0) {
+        myLibrary.forEach((book) => {
+            const tr = document.createElement("tr");
+            for (const prop in book) {
+                const td = document.createElement("td");
+                td.innerText = book[prop];
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        });
+    } else {
+        const td = document.createElement("td");
+        td.colSpan = keys.length;
+        td.innerText = "Add some books to your library!";
+        table.appendChild(document.createElement("tr")).appendChild(td);
+    }
 }
-console.log(myLibrary);
+
+/* 
+█▄ ▄█ ▄▀▄ █▀▄ ▄▀▄ █   
+█ ▀ █ ▀▄▀ █▄▀ █▀█ █▄▄  */
+const modalAddBook = document.querySelector("#modal-addBook");
+
+//SUBMIT
+const btnAddBookSubmit = document.querySelector("#addBookSubmit");
+btnAddBookSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    const author = document.querySelector("#author");
+    const title = document.querySelector("#title");
+    const status = document.querySelector("#status");
+    myLibrary.push(new Book(author.value, title.value, status.value));
+    displayBooks();
+    closeModal(modalAddBook);
+});
+
+//CLOSE
+const btnModalClose = document.querySelector("#btnModalClose");
+btnModalClose.addEventListener("click", (event) => {
+    closeModal(modalAddBook);
+});
+closeModal = (modal) => {
+    modal.querySelector("form").reset();
+    modal.remove();
+};
+closeModal(modalAddBook);
+
+/* 
+██▄ █ █ ▀█▀ ▀█▀ ▄▀▄ █▄ █ ▄▀▀ 
+█▄█ ▀▄█  █   █  ▀▄▀ █ ▀█ ▄█▀  */
+//ADD BOOK
+const btnAddBook = document.querySelector("#addBook");
+btnAddBook.addEventListener("click", (event) => {
+    container.appendChild(modalAddBook);
+});
+
+displayBooks();
