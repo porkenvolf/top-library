14aeb3d4982dda41e902e3b67bb67ebdd178b023
Porkenvolf <89871231+porkenvolf@users.noreply.github.com>
2023-06-19 18:48:45 -0300

    Refactor script.js into modular design
    First steps taken, work still needed.

--- js/script.js
+++ js/script.js
@@ -1,159 +1,175 @@
-let myLibrary = [
-    new Book("Harper Lee", "To Kill a Mockingbird", "Read"),
-    new Book("George Orwell", "1984", "Not read"),
-    new Book("J.K. Rowling", "Harry Potter and the Sorcerer's Stone", "Read"),
-    new Book("Jane Austen", "Pride and Prejudice", "Not read"),
-    new Book("F. Scott Fitzgerald", "The Great Gatsby", "Read"),
-    new Book("Markus Zusak", "The Book Thief", "Not read"),
-    new Book("J.R.R. Tolkien", "The Lord of the Rings", "Read"),
-    new Book("Ernest Hemingway", "The Old Man and the Sea", "Not read"),
-    new Book("Agatha Christie", "Murder on the Orient Express", "Read"),
-    new Book(
+let library = (function () {
+    //CACHE DOM
+    const container = document.querySelector(".container");
+    const table = document.querySelector("#books");
+    const btnAddBook = document.querySelector("#addBook");
+
+    //INIT
+    let books = [];
+    newBook("Harper Lee", "To Kill a Mockingbird", "Read", true);
+    newBook("George Orwell", "1984", "Not read", true);
+    newBook(
+        "J.K. Rowling",
+        "Harry Potter and the Sorcerer's Stone",
+        "Read",
+        true
+    );
+    newBook("Jane Austen", "Pride and Prejudice", "Not read", true);
+    newBook("F. Scott Fitzgerald", "The Great Gatsby", "Read", true);
+    newBook("Markus Zusak", "The Book Thief", "Not read", true);
+    newBook("J.R.R. Tolkien", "The Lord of the Rings", "Read", true);
+    newBook("Ernest Hemingway", "The Old Man and the Sea", "Not read", true);
+    newBook("Agatha Christie", "Murder on the Orient Express", "Read", true);
+    newBook(
         "Gabriel Garcia Marquez",
         "One Hundred Years of Solitude",
-        "Not read"
-    ),
-];
+        "Not read",
+        true
+    );
 
-function Book(author, title, status) {
-    // the constructor...
-    this.author = author;
-    this.title = title;
-    this.status = status;
-}
-Book.prototype.delete = function () {
-    myLibrary.splice(myLibrary.indexOf(this), 1);
-};
-Book.prototype.toggleRead = function () {
-    switch (this.status) {
-        case "Read":
-            this.status = "Not read";
-            break;
-        case "Not read":
-            this.status = "Read";
-            break;
-    }
-};
-
-function addBookToLibrary(author, title, status) {
-    myLibrary.push(new Book(author, title, status));
-}
-function removeBookFromLibrary() {}
-function updateBook() {}
-
-/* 
-█ █ ▄▀▀ ██▀ █▀▄    █ █▄ █ ▀█▀ ██▀ █▀▄ █▀ ▄▀▄ ▄▀▀ ██▀ 
-▀▄█ ▄█▀ █▄▄ █▀▄    █ █ ▀█  █  █▄▄ █▀▄ █▀ █▀█ ▀▄▄ █▄▄  */
-const container = document.querySelector(".container");
-const table = document.querySelector("#books");
-
-function displayBooks() {
-    //CLEAR
-    table.innerText = "";
-    //table HEADERS
-    const row = document.createElement("div");
-    row.classList.add("row");
-    const keys = Object.keys(new Book());
-    keys.forEach((element) => {
-        const cell = createCell(element.toUpperCase(), ["header", element]);
-        row.appendChild(cell);
+    //BIND EVENTS
+    btnAddBook.addEventListener("click", (event) => {
+        container.appendChild(modalAddBook);
     });
-    const buttonsHeader = createCell("", ["header"]);
-    row.appendChild(buttonsHeader);
-    table.appendChild(row);
 
-    //table ELEMENTS
-    if (myLibrary.length > 0) {
-        myLibrary.forEach((book) => {
-            const bookRow = document.createElement("div");
-            bookRow.classList.add("row");
-            let propIndex = 1;
-            for (const prop in book) {
-                if (book.hasOwnProperty(prop)) {
-                    const cell = createCell(book[prop], [prop]);
-                    cell.style.gridColumn = propIndex;
-                    propIndex += 1;
+    render();
 
-                    if (prop === "status") {
-                        if (book[prop] === "Read") {
-                            for (const child of cell.children) {
-                                child.classList.add("read");
-                            }
-                        } else if (book[prop] === "Not read") {
-                            for (const child of cell.children) {
-                                child.classList.add("notRead");
+    function newBook(author, title, status, push) {
+        book = { author, title, status };
+        if (push) {
+            books.push(book);
+            render();
+        }
+        return book;
+    }
+
+    function deleteBook(id) {
+        books.splice(id, 1);
+    }
+    function toggleRead(id) {
+        const status = books[id].status;
+        switch (status) {
+            case "Read":
+                books[id].status = "Not read";
+                break;
+            case "Not read":
+                books[id].status = "Read";
+                break;
+        }
+    }
+    function render() {
+        //CLEAR
+        table.innerText = "";
+        //table HEADERS
+        const row = document.createElement("div");
+        row.classList.add("row");
+        const keys = Object.keys(newBook("", "", ""));
+        keys.forEach((element) => {
+            const cell = createCell(element.toUpperCase(), ["header", element]);
+            row.appendChild(cell);
+        });
+        const buttonsHeader = createCell("", ["header"]);
+        row.appendChild(buttonsHeader);
+        table.appendChild(row);
+
+        //table ELEMENTS
+        if (books.length > 0) {
+            books.forEach((book) => {
+                const bookRow = document.createElement("div");
+                bookRow.classList.add("row");
+                let propIndex = 1;
+                for (const prop in book) {
+                    if (book.hasOwnProperty(prop)) {
+                        const cell = createCell(book[prop], [prop]);
+                        cell.style.gridColumn = propIndex;
+                        propIndex += 1;
+
+                        if (prop === "status") {
+                            if (book[prop] === "Read") {
+                                for (const child of cell.children) {
+                                    child.classList.add("read");
+                                }
+                            } else if (book[prop] === "Not read") {
+                                for (const child of cell.children) {
+                                    child.classList.add("notRead");
+                                }
                             }
                         }
+
+                        bookRow.appendChild(cell);
                     }
-
-                    bookRow.appendChild(cell);
                 }
+
+                const outerButtons = document.createElement("div");
+                outerButtons.classList.add("cell");
+                outerButtons.style.gridColumn = propIndex;
+
+                const buttons = renderBooktableIcons(book);
+                outerButtons.appendChild(buttons);
+
+                bookRow.appendChild(outerButtons);
+                table.appendChild(bookRow);
+            });
+        } else {
+            const cell = document.createElement("div");
+            cell.style.gridColumn = "1 / -1";
+            cell.innerText = "Add some books to your library!";
+            table.appendChild(cell);
+        }
+
+        //AUX RENDER FUNCTIONS
+        function createCell(content, classes) {
+            const cell = document.createElement("div");
+            const span = document.createElement("div");
+            span.innerText = content;
+            cell.appendChild(span);
+            if (classes) {
+                classes.forEach((element) => {
+                    cell.classList.add(element);
+                });
             }
+            cell.classList.add("cell");
 
-            const outerButtons = document.createElement("div");
-            outerButtons.classList.add("cell");
-            outerButtons.style.gridColumn = propIndex;
+            return cell;
+        }
+        function renderBooktableIcons(book) {
+            const icons = document.createElement("div");
+            //CHANGE READ STATUS
+            const iconRead = document.createElement("img");
+            iconRead.setAttribute("src", "./svg/read.svg");
+            const buttonRead = document.createElement("button");
+            buttonRead.classList.add("icon");
+            buttonRead.appendChild(iconRead);
+            buttonRead.setAttribute("data-bookindex", books.indexOf(book));
+            buttonRead.addEventListener("click", (event) => {
+                const index = event.target.getAttribute("data-bookindex");
+                toggleRead(index);
+                render();
+            });
 
-            const buttons = renderBooktableIcons(book);
-            outerButtons.appendChild(buttons);
+            //DELETE
+            const iconDelete = document.createElement("img");
+            iconDelete.setAttribute("src", "./svg/delete-forever.svg");
+            const buttonDelete = document.createElement("button");
+            buttonDelete.classList.add("icon");
+            buttonDelete.appendChild(iconDelete);
+            buttonDelete.setAttribute("data-bookindex", books.indexOf(book));
+            buttonDelete.addEventListener("click", (event) => {
+                const index = event.target.getAttribute("data-bookindex");
+                deleteBook(index);
+                render();
+            });
 
-            bookRow.appendChild(outerButtons);
-            table.appendChild(bookRow);
-        });
-    } else {
-        const cell = document.createElement("div");
-        cell.style.gridColumn = "1 / -1";
-        cell.innerText = "Add some books to your library!";
-        table.appendChild(cell);
+            //RETURN
+            icons.appendChild(buttonRead);
+            icons.appendChild(buttonDelete);
+            return icons;
+        }
     }
-}
-function createCell(content, classes) {
-    const cell = document.createElement("div");
-    const span = document.createElement("div");
-    span.innerText = content;
-    cell.appendChild(span);
-    if (classes) {
-        classes.forEach((element) => {
-            cell.classList.add(element);
-        });
-    }
-    cell.classList.add("cell");
 
-    return cell;
-}
-function renderBooktableIcons(book) {
-    const icons = document.createElement("div");
-    //CHANGE READ STATUS
-    const iconRead = document.createElement("img");
-    iconRead.setAttribute("src", "./svg/read.svg");
-    const buttonRead = document.createElement("button");
-    buttonRead.classList.add("icon");
-    buttonRead.appendChild(iconRead);
-    buttonRead.setAttribute("data-bookindex", myLibrary.indexOf(book));
-    buttonRead.addEventListener("click", (event) => {
-        const index = event.target.getAttribute("data-bookindex");
-        myLibrary[index].toggleRead();
-        displayBooks();
-    });
-
-    //DELETE
-    const iconDelete = document.createElement("img");
-    iconDelete.setAttribute("src", "./svg/delete-forever.svg");
-    const buttonDelete = document.createElement("button");
-    buttonDelete.classList.add("icon");
-    buttonDelete.appendChild(iconDelete);
-    buttonDelete.setAttribute("data-bookindex", myLibrary.indexOf(book));
-    buttonDelete.addEventListener("click", (event) => {
-        const index = event.target.getAttribute("data-bookindex");
-        myLibrary[index].delete();
-        displayBooks();
-    });
-
-    //RETURN
-    icons.appendChild(buttonRead);
-    icons.appendChild(buttonDelete);
-    return icons;
-}
+    //API
+    return { newBook };
+})();
 
 /* 
 █▄ ▄█ ▄▀▄ █▀▄ ▄▀▄ █   
@@ -168,8 +184,7 @@ addBookForm.addEventListener("submit", (event) => {
     const author = document.querySelector("#author");
     const title = document.querySelector("#title");
     const status = document.querySelector("#status");
-    myLibrary.push(new Book(author.value, title.value, status.value));
-    displayBooks();
+    library.newBook(author.value, title.value, status.value, true);
     closeModal(modalAddBook);
 });
 
@@ -183,14 +198,3 @@ closeModal = (modal) => {
     modal.remove();
 };
 closeModal(modalAddBook);
-
-/* 
-██▄ █ █ ▀█▀ ▀█▀ ▄▀▄ █▄ █ ▄▀▀ 
-█▄█ ▀▄█  █   █  ▀▄▀ █ ▀█ ▄█▀  */
-//ADD BOOK
-const btnAddBook = document.querySelector("#addBook");
-btnAddBook.addEventListener("click", (event) => {
-    container.appendChild(modalAddBook);
-});
-
-displayBooks();
